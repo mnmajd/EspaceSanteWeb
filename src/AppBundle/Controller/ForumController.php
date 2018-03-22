91168061edc4fb36fbb9f3ce2373597f3d2bc2ff
majdas007 <majdmn6@gmail.com>
2018-03-22 18:00:15 +0100

    integration template mise a jour

---
+++ src/AppBundle/Controller/ForumController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: majd
+ * Date: 22/03/18
+ * Time: 05:20 م
+ */
+
+namespace AppBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ForumController extends Controller
+{
+
+
+}
